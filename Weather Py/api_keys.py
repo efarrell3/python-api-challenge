c96aa87ab1991edbfb40d86c63be95c81d98844b
efarrell3 <69441481+efarrell3@users.noreply.github.com>
2020-10-20 18:25:23 -0400

    Add files via upload

--- Weather Py/api_keys.py
+++ Weather Py/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "836d95834bd4ab0473657995f44bb512"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBfU8UHnBSO-gQJxRH2NVOc4a5kwzmr-u8"
