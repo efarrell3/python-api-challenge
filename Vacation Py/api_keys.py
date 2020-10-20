afdfbb23bbd7fcf871fabac02acb67f369d897ff
efarrell3 <69441481+efarrell3@users.noreply.github.com>
2020-10-20 18:33:47 -0400

    Add files via upload

--- Vacation Py/api_keys.py
+++ Vacation Py/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "836d95834bd4ab0473657995f44bb512"
+weather_api_key = "Your Key Here"
 
 # Google API Key
-g_key = "AIzaSyBfU8UHnBSO-gQJxRH2NVOc4a5kwzmr-u8"
+g_key = "Your Key Here!"
